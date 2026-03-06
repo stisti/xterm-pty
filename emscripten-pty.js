c948410ead39787a50182e31c9ecd543cb6e3d53
Sami Tikka <sti@iki.fi>
2026-03-06 20:48:00 +0200

    Simplify proxying - call PTY function directly
    Remove postMessage proxying approach and just call the function
    directly. The PTY object should be accessible via Module.pty
    from both main thread and worker thread.
    
    This avoids the 'unknown command' error from emscripten's
    worker message handler.

--- emscripten-pty.js
+++ emscripten-pty.js
@@ -87,21 +87,6 @@ Object.assign(Lib, {
 
             _raise(signalCode);
         });
-
-#if PTHREADS && PROXY_TO_PTHREAD
-        // Emscripten 4.0+: Set up message handler for PTY wait operations
-        if (typeof addEventListener !== 'undefined') {
-            addEventListener('message', function(e) {
-                if (e.data && e.data.cmd === 'pty_wait_readable') {
-                    var atomicIndex = e.data.atomicIndex;
-                    PTY_waitForReadableWithCallback(function(type) {
-                        Atomics.store(HEAP32, atomicIndex, type);
-                        Atomics.notify(HEAP32, atomicIndex);
-                    });
-                }
-            });
-        }
-#endif
     `),
 
     $PTY_pollTimeout: 0,
@@ -155,27 +140,11 @@ Object.assign(Lib, {
 
     $PTY_waitForReadableWithAtomicImpl__deps: ['$PTY_waitForReadableWithCallback'],
     $PTY_waitForReadableWithAtomicImpl: (atomicIndex) => {
-#if PROXY_TO_PTHREAD
-        // Emscripten 4.0+: Proxy to main thread using postMessage
-        if (ENVIRONMENT_IS_PTHREAD) {
-            // Send message to main thread
-            postMessage({
-                'cmd': 'pty_wait_readable',
-                'atomicIndex': atomicIndex
-            });
-        } else {
-            // Already on main thread
-            PTY_waitForReadableWithCallback(type => {
-                Atomics.store(HEAP32, atomicIndex, type);
-                Atomics.notify(HEAP32, atomicIndex);
-            });
-        }
-#else
+        // Emscripten 4.0+: Call directly - PTY object should be accessible via Module
         PTY_waitForReadableWithCallback(type => {
             Atomics.store(HEAP32, atomicIndex, type);
             Atomics.notify(HEAP32, atomicIndex);
         });
-#endif
     },
 
     $PTY_atomicIndex: 0,
